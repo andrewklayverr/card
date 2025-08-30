c82edf60a778e99eae368505c61b46099dcee958
andrewklayverr <andrewklayver87@gmail.com>
2025-08-30 15:40:44 -0300

    selection notes

--- my-app/src/components/CardComponent.jsx
+++ my-app/src/components/CardComponent.jsx
@@ -1,9 +1,11 @@
-//import { useState } from "react";
+import { useState } from "react";
+//import { useNavigate } from "react-router-dom";
 import React from "react";
 import "./cardCss.css";
 
 export default function CardComponent() {
-//  const [selectedRating, setSelectedRating] = useState(null);
+  const [selectedRating, setSelectedRating] = useState(null);
+  //const navigate = useNavigate();
 
   return (
     <div className="card">
@@ -14,11 +16,14 @@ export default function CardComponent() {
         appreciated to help us improve our offering!
       </p>
       <div className="rating">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+        <button onClick={() => setSelectedRating(1)}>1</button>
+        <button onClick={() => setSelectedRating(2)}>2</button>
+        <button onClick={() => setSelectedRating(3)}>3</button>
+        <button onClick={() => setSelectedRating(4)}>4</button>
+        <button onClick={() => setSelectedRating(5)}>5</button>
+      </div>
+      <div className="selected-rating">
+        {selectedRating ? `You selected ${selectedRating} out of 5` : "No rating selected"}
       </div>
       <button className="submit">Submit</button>
     </div>
