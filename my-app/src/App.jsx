f4989e465368b58630cb2a7ee5de1602f5386efa
andrewklayverr <andrewklayver87@gmail.com>
2025-09-02 15:38:59 -0300

    finished page thanks

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import CardComponent from "./components/CardComponent";
+import CardThanks from "./components/CardThanks/CardThanks";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CardComponent />} />
+        <Route path="/thankyou" element={<CardThanks />} />
       </Routes>
     </BrowserRouter>
   );
